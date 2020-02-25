b8df384888660bcb011ea1173e1d6831508b971a
Smit Shrestha <smitshrestha101@gmail.com>
2020-02-25 14:37:59 -0600

    added images and fixed some views and layouts

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,17 +4,34 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId=NO_IMAGE_PROVIDED;
+    private  static final int NO_IMAGE_PROVIDED=-1;
 
-    public Word(String defaultTranslation, String miwokTranslation){
-        mDefaultTranslation=defaultTranslation;
-        mMiwokTranslation=miwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
     }
 
-    public String getmDefaultTranslation(){
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId=imageResourceId;
+    }
+
+    public String getmDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-    public String getmMiwokTranslation(){
+    public String getmMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId!=NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,18 +3,23 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words){
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId){
         super(context,0, words );
+        mColorResourceId=colorResourceId;
     }
     @NonNull
     @Override
@@ -29,11 +34,23 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         Word currentWord = getItem(position);
 
+        ImageView imageView= (ImageView) listItemView.findViewById(R.id.image);
+        if(currentWord.hasImage()){
+        imageView.setImageResource(currentWord.getmImageResourceId());
+        imageView.setVisibility(View.VISIBLE);}
+        else{
+            imageView.setVisibility(View.GONE);
+        }
+
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTextView.setText(currentWord.getmMiwokTranslation());
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getmDefaultTranslation());
+
+        View textContainer= listItemView.findViewById(R.id.text_container);
+        int color= ContextCompat.getColor(getContext(),mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
